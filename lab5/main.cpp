6235e99fd38b0a52c8a3446463fec318ec2aff5b
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-12-20 22:40:53 +0200

    Solved naming and magic number issues
    Vector b => Vector exampleVector
    5 => SIZE

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,15 +1,16 @@
 #include "lab5.h"
+#define SIZE 5
 using namespace std;
 
 
 int main()
 {
-    Vector b[5];
-    inputMatrix(b);
-    b->sortColumnsAscendingOrder();
-    outputMatrix(b);
-    b->getSumAboveDiagonal();
-    double geometricAverage = b->geometricAverage();
+    Vector exampleVector[SIZE];
+    inputMatrix(exampleVector);
+    exampleVector->sortColumnsAscendingOrder();
+    outputMatrix(exampleVector);
+    exampleVector->getSumAboveDiagonal();
+    double geometricAverage = exampleVector->geometricAverage();
     cout <<  geometricAverage << endl;
     return 0;
 }
